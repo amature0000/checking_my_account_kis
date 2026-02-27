02cdcb22e359b2b086105dcd8c4ea10ad3b90299
Juhyunsoo <amature0000@naver.com>
2026-02-27 14:48:50 +0900

    KISAuth logging kisBalance ongoing

--- kis_auth.py
+++ kis_auth.py
@@ -40,6 +40,7 @@ class KISAuth:
             return json.load(f)
 
     def _save_token(self, token: str, expired: str):
+        print("[KISAuth] 인증토큰 저장")
         valid_date = datetime.strptime(expired, "%Y-%m-%d %H:%M:%S")
         with open(self.token_file, "w", encoding="utf-8") as f:
             data = {"token": token, "valid-date": valid_date.strftime("%Y-%m-%d %H:%M:%S")}
@@ -57,6 +58,7 @@ class KISAuth:
             now_dt = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 
             if exp_dt > now_dt:
+                print("[KISAuth] 유효한 인증토큰 존재. 해당 토큰 반환")
                 return tkg_tmp["token"]
             return None
         except Exception:
@@ -73,6 +75,7 @@ class KISAuth:
 
         saved_token = self._read_token()
         if saved_token is None:
+            print("[KISAuth] 유효한 인증토큰 없음. 발급 절차 진행")
             url = f"{self.base_url}/oauth2/tokenP"
             res = requests.post(url, data=json.dumps(p), headers=self.headers)
             
@@ -98,6 +101,7 @@ class KISAuth:
         """토큰 유효시간을 체크하고 헤더 반환"""
         n2 = datetime.now()
         if (n2 - self.last_auth_time).seconds >= 86400:
+            print("[KISAuth] 유효시간 만료. 헤더 업데이트")
             self._authenticate()
         return copy.deepcopy(self.headers)
 
